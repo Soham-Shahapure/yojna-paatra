2e243080f57529fbb4a90fdee80342f883135434
Soham-Shahapure <sohamss06@gmail.com>
2026-03-20 21:52:15 +0530

    Created a privacy file for URL

---
+++ android/app/src/main/java/com/cep/yojnapaatra/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cep.yojnapaatra;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cep.yojnapaatra',
+  appName: 'YojnaPaatra',
+  webDir: 'build'
+};
+
+export default config;
